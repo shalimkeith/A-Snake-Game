6b77fb596dddbaa9b7bc402286517e12237dfbb0
Shalim Keith <128365613+shalimkeith@users.noreply.github.com>
2026-04-14 14:52:44 +0500

    Add files via upload
    Completed my game!!!!

---
+++ main.py
@@ -0,0 +1,55 @@
+from turtle import Turtle,Screen
+from snake import Snake
+from food import Food
+from scoreboard import Scoreboard
+import time
+import os
+screen = Screen()
+screen.setup(width = 600, height = 600)
+screen.bgcolor("black")
+screen.title("My Snake Game")
+screen.tracer(0)
+
+starting_positions = [(0,0),(-20,0),(-40,0)]
+
+snake = Snake()
+food = Food()
+scoreboard = Scoreboard()
+
+screen.listen()
+screen.onkey(snake.up,"Up")
+screen.onkey(snake.down,"Down")
+screen.onkey(snake.left,"Left")
+screen.onkey(snake.right,"Right")
+
+game_is_on = True
+
+while game_is_on:
+    screen.update()
+    time.sleep(0.1)
+    snake.move()
+
+    # detect collision with food
+
+    if snake.head.distance(food) < 15:
+        food.refresh()
+        snake.extend()
+        scoreboard.increase_score()
+        scoreboard.update_scoreboard()
+
+    # DETECT COLLISION WITH WALLS
+    if snake.head.xcor() > 280 or snake.head.xcor() < -280 or snake.head.ycor() > 280 or snake.head.ycor() < -280:
+        scoreboard.reset()
+        snake.reset()
+
+    # detect collision with tail
+    for segment in snake.segments[1:]:
+        if snake.head.distance(segment) < 10:
+            scoreboard.reset()
+            snake.reset()
+        elif snake.head.distance(segment) < 10:
+            scoreboard.reset()
+            snake.reset()
+    # if head collides with tail we trigger game_over
+
+screen.mainloop()
